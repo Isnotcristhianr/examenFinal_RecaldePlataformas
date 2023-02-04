eeb0eb323bb5a37d367cd8f7272d0238d843c13d
Cristhian Recalde <recaldecd@gmail.com>
2023-02-04 09:20:53 -0600

    inicio finalizado

--- pacientes/app/Views/ventanas/inicio.php
+++ pacientes/app/Views/ventanas/inicio.php
@@ -16,70 +16,32 @@
     </div>
 </div>
 
-<div  >
-    <video width="750" autoplay muted loop>
+<div  style="position: static; align-items: center; align-content: center; text-align: center; margin: 50px;">
+    <video class="rounded" width="750" autoplay muted loop>
         <source src="/pacientes/assets/imgs/production ID_4487122.mp4" type="video/mp4">
     </video>
 </div>
 
-<!--Mini menu-->
-<!-- informacion  -->
-<div class="container text-dark">
-    <div class="row p-2">
-        <div class="col-md-3">
-            <div class="card text-center">
-                <div class="card-header">
-                    <h3>Api</h3>
-                </div>
-                <div class="card-body">
-                    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.</p>
-                </div>
-                <div class="card-footer text-muted">
-                    <a href="<?= base_url('/api') ?>" class="btn btn-primary">Ver</a>
+<!--Informacion-->
+<div class="container text-center text-light">
+   <!--Botones-->
+    <div class="row">
+        <div class="col-12">
+            <div class="row">
+                <div class="col-12">
+                    <h2>¿Qué deseas hacer?</h2>
                 </div>
             </div>
-        </div>
-        <div class="col-md-3">
-            <div class="card text-center">
-                <div class="card-header">
-                    <h3>Libros</h3>
-                </div>
-                <div class="card-body">
-                    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.</p>
-                </div>
-                <div class="card-footer text-muted">
-                    <a href="<?= base_url('/ListarLibros') ?>" class="btn btn-primary">Ver</a>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-3">
-            <div class="card text-center">
-                <div class="card-header">
-                    <h3>Estudiantes</h3>
-                </div>
-                <div class="card-body">
-                    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.</p>
-                </div>
-                <div class="card-footer text-muted">
-                    <a href="<?= base_url('/VerEstudiantes') ?>" class="btn btn-primary">Ver</a>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-3">
-            <div class="card text-center">
-                <div class="card-header">
-                    <h3>Relaciones</h3>
-                </div>
-                <div class="card-body">
-                    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quae.</p>
-                </div>
-                <div class="card-footer text-muted">
-                    <a href="<?= base_url('/relaciones') ?>" class="btn btn-primary">Ver</a>
+            <div class="row">
+                <div class="col-12">
+                    <a id="btn" href="http://localhost/examenfinal_recalde/pacientes/index.php/pacientes" class="btn btn-primary">Ver Pacientes</a>
+                    <a id="btn" href="/pacientes/imc" class="btn btn-primary">Controlar IMC</a>
                 </div>
             </div>
         </div>
     </div>
 </div>
+<br>
 
 <?php
     echo $pie;
