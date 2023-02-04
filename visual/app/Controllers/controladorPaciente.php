5ac3ab05575c883feb91b6fa003defc5c8b142b6
Cristhian Recalde <recaldecd@gmail.com>
2023-02-03 23:53:29 -0600

    index finalizado plantilla

--- visual/app/Controllers/controladorPaciente.php
+++ visual/app/Controllers/controladorPaciente.php
@@ -5,4 +5,17 @@ namespace App\Controllers;
 use CodeIgniter\Controller;
 use App\Model\modeloPaciente;
 
+class controladorPaciente extends Controller{
+    
+    //importar js y cs
+    public function index(){
+      //  $this -> load -> helper('url');
+       // $this -> load -> helper('test');
+    }
+
+    public function inicio(){
+        
+    }
+}
+
 ?>
